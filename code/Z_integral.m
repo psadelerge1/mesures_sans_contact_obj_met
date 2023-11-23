7f405e8ec9ca827160b4055d8b281180d93224da
psadelerge1 <98813363+psadelerge1@users.noreply.github.com>
2023-11-23 11:47:03 +0100

    fminsearch v2 23/11/2023

--- code/Z_integral.m
+++ code/Z_integral.m
@@ -43,7 +43,7 @@ format long
   thick4n = cup(3)/rbar; l4=cup(4)/rbar;
 %----------------------------------------------------------
 % Constantes de propagation pour toutes les frequences
-  puls = 2*pi*Freq*1000;  %w
+  puls = 2*pi*Freq;  %w
   csten = rbar*rbar*1E-6; %  § § §
   w2ue0 = csten*puls.*puls*mu0*eps0;
   wusr1 = csten*puls*mu0*sig(1);
@@ -78,7 +78,7 @@ end
  % xjr2m1 = (1/x^3) * integrale( J1(z) z dz )
  %		dans l'intervalle  [z1=x*r1 , z2=x*r2]
  f = @(zz) besselj(1,zz).*zz;
- size(x)
+ size(x);
  for k=1:length(x) 
 %   xjr2m1(k) = (1/x(k)^3)* (-0.5.*pi.*(x(k)*r2.*((besselj(0,x(k)*r2).*StruveH1(x(k)*r2)-besselj(1,x(k)*r2).*StruveH0(x(k)*r2)))-x(k)*r1.*((besselj(0,x(k)*r1).*StruveH1(x(k)*r1)-besselj(1,x(k)*r1).*StruveH0(x(k)*r1)))));
 %   (-0.5.*pi.*(a.*2.*((besselj(0,a.*2).*StruveH1(a.*2)-besselj(1,a.*2).*StruveH0(a.*2)))-a.*((besselj(0,a).*StruveH1(a)-besselj(1,a).*StruveH0(a)))))
--- code/fminsearch_test_2.m
+++ code/fminsearch_test_2.m
@@ -3,11 +3,14 @@ fopen(s);
 fprintf(s,'*TRG');
 fprintf(s,'OUTPUT,ON');
 fprintf(s,'FREQUE,33e3');
+%{
 m =3;
+
 for n = 1:m
     fprintf(s,'BEEP');
     pause(0.5);
 end
+%}
 fprintf(s,'LCR?');
 val = fscanf(s);
 
@@ -21,22 +24,22 @@ turn    = 20;     %nombre de spires
 coil = [r1 r2 l3 turn];
 
 sigma     = 0;     %conductivite du couvercle
-mu_r      = 1000;     %permeabilite du couvercle
+mu_r      = 10000;     %permeabilite du couvercle
 epaisseur = 2.21;     %epaisseur du couvercle
-l4        = 0;     %distance bobine au couvercle
+l4        = 0.1;     %distance bobine au couvercle
 cup =[sigma,mu_r,epaisseur,l4];
 
-c1=20.1e6; %Conductivites m1
+c1_1=0.61e6; %Conductivites m1
 c2 = 0 ; %Conductivites m2
-sig = [c1 c2];
+sig = [c1_1 c2];
 
 mu_1=1; %Permeabilites relatives dans les milieux 1 et 2
 mu_2=1;
 mu =[mu_1 mu_2];
 
-Freq = 33; %en kHz
-t1 = 25;  %Epaisseur de la plaque conductrice en mm
-l0 = 0.1;  %Distance capteur-cible en mm
+%Freq = 33; %en kHz
+t1 = 20;  %Epaisseur de la plaque conductrice en mm
+l0 = 0;  %Distance capteur-cible en mm
 
 
 
@@ -51,12 +54,13 @@ valnum(x) =
 10 : inductance parallel || 13 : Q factor
 %}
 
-Freq = valnum(1)/1000;%on récup val IAI
-omeg = 2*pi*Freq*1000;
-Res  = valnum(6);
+Freq = valnum(1);%on récup val IAI
+omeg = 2*pi*Freq;
+Res  = valnum(6)-0.075
 Ind  = valnum(7);
-Z_mes = Res+Ind*omeg*j
-val_integral = Z_integral(coil,Freq,t1,l0,sig,mu,cup)
+Z_mes = Res+Ind*omeg*j;
+R_sim =real(Z_integral(coil,Freq,t1,l0,[0.61,0],mu,cup))-0.075
+val_integral = Z_integral(coil,Freq,t1,l0,sig,mu,cup);
 %delta_Indu=Ind-imag(val_integral)/(2*pi*Freq*1000)
 %Ind_mes = Ind
 %Ind_sim =imag(val_integral)/(2*pi*Freq*1000)
@@ -75,7 +79,11 @@ cup
 
 
 f=@(c1)abs(Z_integral(coil,Freq,t1,l0,[c1,0],mu,cup)-Z_mes)^2;
-%f=@(c1)(Ind-(imag(Z_integral(coil,Freq,t1,l0,[c1,0],mu,cup))/(2*pi*Freq*1000)))+(Res-real(Z_integral(coil,Freq,t1,l0,[c1,0],mu,cup)));
+%I_ressssss=abs(Z_integral(coil,Freq,t1,l0,[0.61,0],mu,cup)-Z_mes)^2
+%abs_test_sim = abs(Z_integral(coil,Freq,t1,l0,[20.1e6,0],mu,cup))^2
+%abs_test_mes = abs(Z_mes)^2
+%f=@(c1)abs(Z_integral(coil,Freq,t1,l0,[c1,0],mu,cup)-Z_integral(coil,Freq,t1,l0,[20.1e6,0],mu,cup))^2;
+%f=@(c1)(Ind-(imag(Z_integral(coil,Freq,t1,l0,[c1,0],mu,cup))/(2*pi*Freq*1000)))+(Res-0.078-real(Z_integral(coil,Freq,t1,l0,[c1,0],mu,cup)));
 fun = @(c1)f(c1);
 
 %{
@@ -84,7 +92,7 @@ Z_integral(coil,Freq,t1,l0,[c1,0],mu,cup)
 ABS=abs(Z_mes-(Z_integral(coil,Freq,t1,l0,[c1,0],mu,cup)))^2
 %}
 
-c1_0=5.1e6;
+c1_0=5e6;
 options = optimset('PlotFcns',@optimplotfval);
 
 c1= fminsearch(fun,c1_0,options)
