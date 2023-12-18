b5e333a0ab298d6632eedb34b1c087b61abac81e
callafge1 <98813313+callafge1@users.noreply.github.com>
2023-12-18 16:04:54 +0100

    ok

--- code/Interface2.m
+++ code/Interface2.m
@@ -1,4 +1,3 @@
-
 function varargout = Interface2(varargin)
 % INTERFACE2 MATLAB code for Interface2.fig
 %      INTERFACE2, by itself, creates a new INTERFACE2 or raises the existing
@@ -43,6 +42,7 @@ else
     gui_mainfcn(gui_State, varargin{:});
 end
 % End initialization code - DO NOT EDIT
+
 s = serial('COM1','BaudRate',19200,'Terminator','CR/LF'); %Create a serial port 
 
 % --- Executes just before Interface2 is made visible.
@@ -79,24 +79,15 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-s = serial('COM1','BaudRate',19200,'Terminator','CR/LF'); %Create a serial port  
+s = serial('COM1','BaudRate',19200,'Terminator','CR/LF');  
 fopen(s);
-
 fprintf(s,'*TRG');
 fprintf(s,'OUTPUT,ON');
 Freq_str =3.3e3;
 fprintf(s,'*TRG');
 fprintf(s,'OUTPUT,ON');
 fprintf(s,['FREQUE,', num2str(Freq_str)]);
-
-%m =3;
-%for n = 1:m
-%    fprintf(s,'BEEP');
- %   pause(0.25);
-%end
 fprintf(s,'LCR?');
-
-
 fclose(s);
 msgbox('Réglage PSM OK')
 
@@ -157,8 +148,6 @@ fprintf(s,'OUTPUT,ON');
 fprintf(s,'LCR?');
 val = fscanf(s);
 valnum = str2num(val);
-
-
 r1  = 10.23;        %rayon interieur bobine d = 20.46
 r2  = 21.315;       %rayon exterieur bobine d = 42.63 mm
 l3      = 2.45;   %hauteur bobine 
@@ -175,24 +164,20 @@ sig = [c1_1 c2];
 mu_1=1; %Permeabilites relatives dans les milieux 1 et 2
 mu_2=1;
 mu =[mu_1 mu_2];
-
 choice = menu('Regler :','Hauteur');
 prompt = {'Hauteur en mm :'};
 dlg_title = 'Input';
 num_lines = 1;
 answer = inputdlg(prompt,dlg_title,num_lines);
 x = answer{1};
-
 t1 = x;  %Epaisseur de la plaque conductrice en mm
 l0 = 0;  %Distance capteur-cible en mm
-
 Freq = valnum(1);%on récup val IAI
 omeg = 2*pi*Freq;
 Res  = valnum(6)-0.075;
 Ind  = valnum(7);
 Z_mes = Res+Ind*omeg*j;
 sig_freq=1.900666666666667e+10;
-
 f=@(c1)abs(Z_integral(coil,Freq/1000,t1,l0,[c1,0],mu,cup)-Z_mes)^2;
 fun = @(c1)f(c1);
 c1_0=5e6;
@@ -200,28 +185,21 @@ f1_0 = 3.5e3;
 options = optimset('PlotFcns',@optimplotfval,'MaxIter',10);
 c_res= fminsearch(fun,c1_0,options)
 N_Freq=sig_freq/c_res;
-
 fprintf(s,['FREQUE,', num2str(N_Freq)]);
 fprintf(s,'*TRG');
 pause(2);
 fprintf(s,'LCR?');
 val = fscanf(s);
 valnum = str2num(val);
-
 omeg = 2*pi*N_Freq;
-
 Res  = valnum(6)-0.075;
 Ind  = valnum(7);
-
 Z_mes_2 = Res+Ind*omeg*j;
-
 f2=@(c1)abs(Z_integral(coil,N_Freq/1000,t1,l0,[c1,0],mu,cup)-Z_mes_2)^2;
 fun_2 = @(c1)f2(c1);
 c_res_2= fminsearch(fun_2,c1_0,options)
 cond=1.4e6
-
 fclose(s);
-
 set(handles.edit5,'string',valnum(7));
 pause(0.5);
 set(handles.edit6,'string',valnum(6));
