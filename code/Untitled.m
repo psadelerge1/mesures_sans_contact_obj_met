0f9d71df25b77e0ed2536007fb2c2ea2e82857bd
callafge1 <98813313+callafge1@users.noreply.github.com>
2023-12-19 11:25:17 +0100

    ok

--- code/Untitled.m
+++ code/Untitled.m
@@ -1,7 +1,21 @@
-choice = menu('calc method','J','K 2007','h program');
+choice = menu('Choose an operation', 'Plus', 'Minus', ... 
+    'Multiply', 'Divide');
 
-prompt = {'Enter value for x','Enter value for y'};
+prompt = {'Enter value for x', 'Enter value for y'};
 dlg_title = 'Input';
 num_lines = 1;
-def = {'',''};
-answer = inputdlg(prompt,dlg_title);
+def = {'', ''};
+answer = inputdlg(prompt, dlg_title, num_lines, def);
+
+x = str2num(answer{1});
+y = str2num(answer{2});
+
+if choice == 1
+    result = x + y;
+elseif choice == 2
+    result = x - y;
+elseif choice == 3
+    result = x*y;
+else 
+    result = x/y;  
+end
