a80eee89108323ec4d382ee2bca262475ee5ad2c
psadelerge1 <98813363+psadelerge1@users.noreply.github.com>
2023-12-21 11:38:07 +0100

    Update des commenataire
    Quasiment fini, a continuer

--- code/Interface2.m
+++ code/Interface2.m
@@ -185,49 +185,49 @@ function pushbutton4_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton4 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-s = serial('COM1','BaudRate',19200,'Terminator','CR/LF'); %Create a serial port  
-fopen(s);
+s = serial('COM1','BaudRate',19200,'Terminator','CR/LF'); %Créer un port COM1 à un débit de 19200 bauds
+fopen(s); % Ouverture de la laisaion 
 
-fprintf(s,'*TRG');
-fprintf(s,'OUTPUT,ON');
-fprintf(s,'LCR?');
+fprintf(s,'*TRG');%Configure TRG du PSM
+fprintf(s,'OUTPUT,ON'); %Configure OUTPUT du PSM
+fprintf(s,'LCR?');%Configure LCR du PSM
 val = fscanf(s);
 valnum = str2num(val);
 
-r1  = 10.23;        %rayon interieur bobine d = 20.46
-r2  = 21.315;       %rayon exterieur bobine d = 42.63 mm
-l3      = 2.45;   %hauteur bobine 
-turn    = 20;     %nombre de spires
+r1  = 10.23;        %Rayon interieur bobine d = 20.46
+r2  = 21.315;       %Rayon exterieur bobine d = 42.63 mm
+l3      = 2.45;   %Hauteur bobine 
+turn    = 20;     %Nombre de spires
 coil = [r1 r2 l3 turn];
-sigma     = 0;     %conductivite du couvercle
-mu_r      = 1000;     %permeabilite du couvercle
-epaisseur = 2.21;     %epaisseur du couvercle
-l4        = 0.1;     %distance bobine au couvercle
+sigma     = 0;     %Conductivite du couvercle
+mu_r      = 1000;     %Permeabilite du couvercle
+epaisseur = 2.21;     %Epaisseur du couvercle
+l4        = 0.1;     %Distance bobine au couvercle
 cup =[sigma,mu_r,epaisseur,l4];
 
 
 c1_1=0.61e6; %Conductivites m1
 c2 = 0 ; %Conductivites m2
-sig = [c1_1 c2];
+sig = [c1_1 c2];%////
 mu_1=1; %Permeabilites relatives dans les milieux 1 et 2
-mu_2=1;
+mu_2=1;%////
 mu =[mu_1 mu_2];
 
-choice = menu('Regler :','Hauteur');
-prompt = {'Hauteur en mm :'};
-dlg_title = 'Input';
-num_lines = 1;
+choice = menu('Regler :','Hauteur'); %Affichage de la fenetre "REGLER"
+prompt = {'Hauteur en mm :'}; %Affichage du texte sur la fenetre precedente
+dlg_title = 'Input'; %Creation d'une entrer de valeurs 
+num_lines = 1; %Valeurs sur 1 ligne 
 answer = inputdlg(prompt,dlg_title,num_lines);
-x = answer{1};
+x = answer{1}; %Attribue X a la valeurs ecrite 
 
-t1=str2num(x);%Epaisseur de la plaque conductrice en mm
+t1=str2num(x);%Epaisseur de la plaque conductrice en mm avec conversion d'un string en valeur numérique 
 l0 = 0;  %Distance capteur-cible en mm
 
-Freq = valnum(1);%on récup val IAI
-omeg = 2*pi*Freq;
-Res  = valnum(6)-0.075;
-Ind  = valnum(7);
-Z_mes = Res+Ind*omeg*j;
+Freq = valnum(1);%On récup val IAI
+omeg = 2*pi*Freq;%Formule de la pulsation
+Res  = valnum(6)-0.075;%Attribue la valeurs de la ressiatnce du PSM a la variable Res 
+Ind  = valnum(7); %Atribue la valeur de l'inductance du PSM a la variable Ind
+Z_mes = Res+Ind*omeg*j; %Formule de la conductivite mesure
 sig_freq=1.900666666666667e+10;
 f=@(c1)abs(Z_integral(coil,Freq/1000,t1,l0,[c1,0],mu,cup)-Z_mes)^2;
 fun = @(c1)f(c1);
@@ -235,43 +235,46 @@ fun = @(c1)f(c1);
 c1_0=5e6;
 f1_0 = 3.5e3;
 
-options = optimset('PlotFcns',@optimplotfval,'MaxIter',10);
-c_res= fminsearch(fun,c1_0,options)
+options = optimset('PlotFcns',@optimplotfval,'MaxIter',10); %Limite le nombre ittération pendant les mesures 
+c_res= fminsearch(fun,c1_0,options)%Ajuste les paramètres en fonction des mesures obtenues
 N_Freq=sig_freq/c_res;
 
-fprintf(s,['FREQUE,', num2str(N_Freq)]);
-fprintf(s,'*TRG');
-pause(2);
-fprintf(s,'LCR?');
+fprintf(s,['FREQUE,', num2str(N_Freq)]); %Affichage de la frequence apres optimisation
+fprintf(s,'*TRG');%Configure TRG du PSM
+pause(2); %Attente de 2 secondes 
+fprintf(s,'LCR?'); %Configure LCR du PSM
 
 val = fscanf(s);
-valnum = str2num(val);
+valnum = str2num(val); %Conversion d'un string en valeur numérique 
 
-omeg = 2*pi*N_Freq;
-Res  = valnum(6)-0.075;
-Ind  = valnum(7);
-Z_mes_2 = Res+Ind*omeg*j;
+omeg = 2*pi*N_Freq; %Formule de la pulsation
+Res  = valnum(6)-0.075;%Attribue la valeurs de la ressiatnce du PSM a la variable Res 
+Ind  = valnum(7); %Attribue la valeurs de l'inductance du PSM a la variable Ind 
+Z_mes_2 = Res+Ind*omeg*j;%Formule de la conductivite mesure une deuxieme fois 
 f2=@(c1)abs(Z_integral(coil,N_Freq/1000,t1,l0,[c1,0],mu,cup)-Z_mes_2)^2;
 fun_2 = @(c1)f2(c1);
-c_res_2= fminsearch(fun_2,c1_0,options)
+c_res_2= fminsearch(fun_2,c1_0,options)%Ajuste les paramètres en fonction des mesures obtenues une deuxieme fois
 cond=1.4e6
 
-fclose(s);
+fclose(s); %Fermeture de la liaison avce le PSM et IAI
 
-set(handles.edit5,'string',valnum(7));
-pause(0.5);
-set(handles.edit6,'string',valnum(6));
-pause(0.5);
-set(handles.edit8,'string',c_res_2);
-pause(0.5);
-set(handles.edit9,'string',N_Freq(1));
+set(handles.edit5,'string',valnum(7)); %Attribue les valeurs de l'inductance du PSM sur les "EDIT" pour l'affichage 
+pause(0.5);%Attente de 0.5 secondes 
+set(handles.edit6,'string',valnum(6));%Attribue les valeurs de la resistance du PSM sur les "EDIT" pour l'affichage 
+pause(0.5);%Attente de 0.5 secondes 
+set(handles.edit8,'string',c_res_2);%Attribue les valeurs de la conductivite du PSM sur les "EDIT" pour l'affichage 
+pause(0.5);%Attente de 0.5 secondes 
+set(handles.edit9,'string',N_Freq(1));%Attribue les valeurs de la fréquence du PSM sur les "EDIT" pour l'affichage 
 
 
 % --- Executes on button press in pushbutton5.
 function pushbutton5_Callback(hObject, eventdata, handles)
-s = serial('COM1','BaudRate',19200,'Terminator','CR/LF'); %Create a serial port  
-fopen(s);
+s = serial('COM1','BaudRate',19200,'Terminator','CR/LF'); %Créer un port COM1 à un débit de 19200 bauds
+fopen(s); % Ouverture de la laisaion 
 
+fprintf(s,'*TRG');%Configure TRG du PSM
+fprintf(s,'OUTPUT,ON'); %Configure OUTPUT du PSM
+fprintf(s,'LCR?');%Configure LCR du PSM
 fprintf(s,'*TRG');
 fprintf(s,'OUTPUT,ON');
 fprintf(s,'LCR?');
@@ -279,41 +282,41 @@ fprintf(s,'LCR?');
 val = fscanf(s);
 valnum = str2num(val);
 
-r1  = 4.35;        %rayon interieur bobine 
-r2  = 8.25;       %rayon exterieur bobine 
+r1  = 4.35;        %Rayon interieur bobine 
+r2  = 8.25;       %Rayon exterieur bobine 
 
-l3      = 0.4;   %hauteur bobine 
-turn    = 22;     %nombre de spires
+l3      = 0.4;   %Hauteur bobine 
+turn    = 22;     %Nombre de spires
 coil = [r1 r2 l3 turn];
 
 sigma     = 0;     %conductivite du couvercle
-mu_r      = 1000;     %permeabilite du couvercle
-epaisseur = 0.4;     %epaisseur du couvercle
-l4        = 0;     %distance bobine au couvercle
+mu_r      = 1000;     %Permeabilite du couvercle
+epaisseur = 0.4;     %Epaisseur du couvercle
+l4        = 0;     %Distance bobine au couvercle
 cup =[sigma,mu_r,epaisseur,l4];
 
 c1_1=0.61e6; %Conductivites m1
 c2 = 0 ; %Conductivites m2
-sig = [c1_1 c2];
+sig = [c1_1 c2];%////
 mu_1=1; %Permeabilites relatives dans les milieux 1 et 2
-mu_2=1;
+mu_2=1;%////
 mu =[mu_1 mu_2];
 
-choice = menu('Regler :','Hauteur');
-prompt = {'Hauteur en mm :'};
-dlg_title = 'Input';
-num_lines = 1;
+choice = menu('Regler :','Hauteur'); %Affichage de la fenetre "REGLER"
+prompt = {'Hauteur en mm :'}; %Affichage du texte sur la fenetre precedente
+dlg_title = 'Input'; %Creation d'une entrer de valeurs 
+num_lines = 1; %Valeurs sur 1 ligne 
 answer = inputdlg(prompt,dlg_title,num_lines);
-x = answer{1};
+x = answer{1}; %Attribue X a la valeurs ecrite 
 
-t1=str2num(x);%Epaisseur de la plaque conductrice en mm
+t1=str2num(x);%Epaisseur de la plaque conductrice en mm avec conversion d'un string en valeur numérique 
 l0 = 0;  %Distance capteur-cible en mm
 
-Freq = valnum(1);%on récup val IAI
-omeg = 2*pi*Freq;
-Res  = valnum(6)-0.075;
-Ind  = valnum(7);
-Z_mes = Res+Ind*omeg*j;
+Freq = valnum(1);%On récup val IAI
+omeg = 2*pi*Freq;%Formule de la pulsation
+Res  = valnum(6)-0.075;%Attribue la valeurs de la ressiatnce du PSM a la variable Res 
+Ind  = valnum(7); %Atribue la valeur de l'inductance du PSM a la variable Ind
+Z_mes = Res+Ind*omeg*j; %Formule de la conductivite mesure
 sig_freq=1.900666666666667e+10;
 f=@(c1)abs(Z_integral(coil,Freq/1000,t1,l0,[c1,0],mu,cup)-Z_mes)^2;
 fun = @(c1)f(c1);
@@ -321,36 +324,36 @@ fun = @(c1)f(c1);
 c1_0=5e6;
 f1_0 = 3.5e3;
 
-options = optimset('PlotFcns',@optimplotfval,'MaxIter',10);
-c_res= fminsearch(fun,c1_0,options)
+options = optimset('PlotFcns',@optimplotfval,'MaxIter',10); %Limite le nombre ittération pendant les mesures 
+c_res= fminsearch(fun,c1_0,options)%Ajuste les paramètres en fonction des mesures obtenues
 N_Freq=sig_freq/c_res;
 
-fprintf(s,['FREQUE,', num2str(N_Freq)]);
-fprintf(s,'*TRG');
-pause(2);
-fprintf(s,'LCR?');
+fprintf(s,['FREQUE,', num2str(N_Freq)]); %Affichage de la frequence apres optimisation
+fprintf(s,'*TRG');%Configure TRG du PSM
+pause(2); %Attente de 2 secondes 
+fprintf(s,'LCR?'); %Configure LCR du PSM
 
 val = fscanf(s);
-valnum = str2num(val);
+valnum = str2num(val); %Conversion d'un string en valeur numérique 
 
-omeg = 2*pi*N_Freq;
-Res  = valnum(6)-0.075;
-Ind  = valnum(7);
-Z_mes_2 = Res+Ind*omeg*j;
+omeg = 2*pi*N_Freq; %Formule de la pulsation
+Res  = valnum(6)-0.075;%Attribue la valeurs de la ressiatnce du PSM a la variable Res 
+Ind  = valnum(7); %Attribue la valeurs de l'inductance du PSM a la variable Ind 
+Z_mes_2 = Res+Ind*omeg*j;%Formule de la conductivite mesure une deuxieme fois 
 f2=@(c1)abs(Z_integral(coil,N_Freq/1000,t1,l0,[c1,0],mu,cup)-Z_mes_2)^2;
 fun_2 = @(c1)f2(c1);
-c_res_2= fminsearch(fun_2,c1_0,options)
+c_res_2= fminsearch(fun_2,c1_0,options)%Ajuste les paramètres en fonction des mesures obtenues une deuxieme fois
 cond=1.4e6
 
-fclose(s);
+fclose(s); %Fermeture de la liaison avce le PSM et IAI
 
-set(handles.edit5,'string',valnum(7));
-pause(0.5);
-set(handles.edit6,'string',valnum(6));
-pause(0.5);
-set(handles.edit8,'string',c_res_2);
-pause(0.5);
-set(handles.edit9,'string',N_Freq(1));
+set(handles.edit5,'string',valnum(7)); %Attribue les valeurs de l'inductance du PSM sur les "EDIT" pour l'affichage 
+pause(0.5);%Attente de 0.5 secondes 
+set(handles.edit6,'string',valnum(6));%Attribue les valeurs de la resistance du PSM sur les "EDIT" pour l'affichage 
+pause(0.5);%Attente de 0.5 secondes 
+set(handles.edit8,'string',c_res_2);%Attribue les valeurs de la conductivite du PSM sur les "EDIT" pour l'affichage 
+pause(0.5);%Attente de 0.5 secondes 
+set(handles.edit9,'string',N_Freq(1));%Attribue les valeurs de la fréquence du PSM sur les "EDIT" pour l'affichage
 
 
 function edit10_Callback(hObject, eventdata, handles)
